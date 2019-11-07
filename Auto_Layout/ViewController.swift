6730f05f7dd3336076f010c5b10c32a5f07faff4
Tiffany Obi <tiffanyobi123@gmail.com>
2019-11-07 12:42:10 -0500

    Initial Commit

---
+++ Auto_Layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto_Layout
+//
+//  Created by Tiffany Obi on 11/7/19.
+//  Copyright © 2019 Tiffany Obi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
